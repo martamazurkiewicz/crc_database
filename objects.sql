fb87bed167209896cb084765bc93f26e9ebeaf0a
martamazurkiewicz <marta@mazurkiewi.cz>
2022-05-20 19:50:36 +0200

    Trigger compiled

--- objects.sql
+++ objects.sql
@@ -8,14 +8,15 @@ CREATE OR REPLACE TRIGGER main.reduce_avail_spots_tg
     
 CREATE OR REPLACE PROCEDURE main.reduce_avail_spots(in_new_part_num IN NUMBER, in_old_part_num IN NUMBER, in_offer_id IN NUMBER)
 AS
-ln_sports_left NUMBER;
 BEGIN
-    IF in_old_part_num IS NOT NULL THEN
-        UPDATE main.offer SET spots_left = spots_left + in_old_part_num WHERE offer_id = in_offer_id;
-    END;
-    IF in_new_part_num IS NOT NULL THEN
-        UPDATE main.offer SET spots_left = spots_left - in_new_part_num WHERE offer_id = in_offer_id;
-    END;
+    UPDATE main.offer SET spots_left = (CASE 
+        WHEN (in_old_part_num IS NOT NULL AND in_new_part_num IS NOT NULL) THEN 
+            spots_left = spots_left + in_old_part_num- in_new_part_num
+        WHEN (in_old_part_num IS NOT NULL) THEN
+            spots_left + in_old_part_num
+        ELSE spots_left - in_new_part_num
+            END) 
+        WHERE offer_id = in_offer_id;
 EXCEPTION
     WHEN CHECK_CONSTRAINT_VIOLATED THEN
         DBMS_OUTPUT.PUT_LINE('Number of participants in reservation is above the number of still available spots for offer with id ' || in_offer_id;
